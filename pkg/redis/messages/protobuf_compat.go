7f5296ef77947f5342e45989fd9bbfd88ff650ab
Ella Fox <ella@fox.gal>
2021-08-09 14:11:29 +0200

    Player type

--- pkg/redis/messages/protobuf_compat.go
+++ pkg/redis/messages/protobuf_compat.go
@@ -2,10 +2,26 @@ package messages
 
 import "google.golang.org/protobuf/proto"
 
-func (x *LoginBegin) MarshalBinary() ([]byte, error) {
+func (x *JavaLoginBegin) MarshalBinary() ([]byte, error) {
 	return proto.Marshal(x)
 }
 
-func (x *LoginBegin) UnmarshalBinary(data []byte) error {
+func (x *JavaLoginBegin) UnmarshalBinary(data []byte) error {
 	return proto.Unmarshal(data, x)
+}
+
+func (x *JavaLoginBegin) UnmarshalBinaryString(data string) error {
+	return proto.Unmarshal([]byte(data), x)
+}
+
+func (x *Player) MarshalBinary() ([]byte, error) {
+	return proto.Marshal(x)
+}
+
+func (x *Player) UnmarshalBinary(data []byte) error {
+	return proto.Unmarshal(data, x)
+}
+
+func (x *Player) UnmarshalBinaryString(data string) error {
+	return proto.Unmarshal([]byte(data), x)
 }
